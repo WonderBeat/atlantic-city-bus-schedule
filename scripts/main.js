10a9e00c0160cc7b38175f6dfcea42ac6714c3b3
Anton Pavlenko <anpav.spb@gmail.com>
2013-08-18 23:56:17 +0400

    Icons 16x16, 48x48, 128x128 were added; main.less file was added to application; More button was added to application

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,14 @@ var busScheduleApp = angular.module('busScheduleApp', []);
 busScheduleApp.controller('ScheduleCtrl', function($scope, $timeout) {
 
     $scope.noBusTodayText = "Сегодня автобусов больше не будет";
+    $scope.moreButtonText = "еще...";
 
+    $scope.departuresLimit = 3;
     $scope.departures = JSON.parse(localStorage.getItem('departure'));
+
+    $scope.increaseDeparturesLimit = function() {
+        $scope.departuresLimit++;
+    };
 
     $scope.$watch('departures', function() {
         $timeout(function() {
